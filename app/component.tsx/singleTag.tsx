8c362019571d96f3fa76650a119e79fed4ba0a6d
salman <salmanbinsalahudeen@gmail.com>
2024-12-12 11:30:24 +0530

    fix(expansion): expansion state persist

--- app/component.tsx/singleTag.tsx
+++ app/component.tsx/singleTag.tsx
@@ -29,7 +29,7 @@ export default function SingleTag({ item }: { item: Props }) {
               data: "Data",
               children: null,
               id: data.id,
-            }, // Ensure 'children' is included if applicable
+            },
           ],
         }));
       })
@@ -50,15 +50,24 @@ export default function SingleTag({ item }: { item: Props }) {
       });
   };
 
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  const handleOnBur = () => {
+    setIsEditing(false);
+    if (localData !== item.data) {
+      updateTag(tags.id as number, tags.name, localData as string); // Save the updated data
+    }
+  };
+
   return (
     <div className="border-2 border-blue-400 rounded-lg bg-blue-200 p-2 mb-2">
       <div className="flex justify-between items-center">
         <div className="flex gap-2">
           <button
             className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
-            onClick={() => {
-              setIsExpanded((prev) => !prev);
-            }}
+            onClick={toggleExpanded}
           >
             <ArrowIcon className={`${isExpanded && "transform rotate-90"}`} />
           </button>
@@ -73,50 +82,40 @@ export default function SingleTag({ item }: { item: Props }) {
           Add Child
         </button>
       </div>
-      {isExpanded && (
-        <>
-          {tags.data && (
-            <div className="mt-2">
-              <label className="block text-sm font-medium text-gray-700">
-                Data
-              </label>
-              {isEditing ? (
-                <input
-                  type="text"
-                  className="border-2 border-gray-300 rounded w-full mt-1 p-1 text-black"
-                  value={localData}
-                  onChange={(e) => setLocalData(e.target.value)}
-                  onBlur={() => {
-                    setIsEditing(false);
-                    if (localData !== item.data) {
-                      updateTag(
-                        tags.id as number,
-                        tags.name,
-                        localData as string
-                      ); // Save the updated data
-                    }
-                  }}
-                  autoFocus
-                />
-              ) : (
-                <p
-                  className="border-2 border-gray-300 rounded w-full mt-1 p-1 cursor-pointer text-black"
-                  onClick={() => setIsEditing(true)}
-                >
-                  {localData || "Click to edit"}
-                </p>
-              )}
-            </div>
-          )}
-          {tags.children && tags.children.length > 0 && (
-            <div className="mt-2 pl-4 border-l-2 border-gray-400">
-              {tags.children.map((child, index) => (
-                <SingleTag key={index} item={child} />
-              ))}
-            </div>
-          )}
-        </>
-      )}
+
+      <div className={`${isExpanded ? "visible" : "hidden"}`}>
+        {tags.data && (
+          <div className="mt-2">
+            <label className="block text-sm font-medium text-gray-700">
+              Data
+            </label>
+            {isEditing ? (
+              <input
+                type="text"
+                className="border-2 border-gray-300 rounded w-full mt-1 p-1 text-black"
+                value={localData}
+                onChange={(e) => setLocalData(e.target.value)}
+                onBlur={handleOnBur}
+                autoFocus
+              />
+            ) : (
+              <p
+                className="border-2 border-gray-300 rounded w-full mt-1 p-1 cursor-pointer text-black"
+                onClick={() => setIsEditing(true)}
+              >
+                {localData || "Click to edit"}
+              </p>
+            )}
+          </div>
+        )}
+        {tags.children && tags.children.length > 0 && (
+          <div className="mt-2 pl-4 border-l-2 border-gray-400">
+            {tags.children.map((child, index) => (
+              <SingleTag key={index} item={child} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
